6feea04d933c210edf01c1e8343b422fcf116b8d
kaydeemoney <daramola4082@gmail.com>
2023-12-13 00:51:13 -0800

    ok

---
+++ yukd_shell.h
@@ -0,0 +1,24 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+/*
+ * We are trying to define all the
+ * header we use in the shell
+ * some functions that include system calls
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+
+
+/* q1_shell.c */
+int one_arg_xcute(void);
+
+#endif
